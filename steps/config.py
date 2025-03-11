9f4473c56172d7521538c94713d472cd3bc7304c
Jakub <124591894+JakubTabor@users.noreply.github.com>
2025-03-11 23:48:22 +0000

    Update config.py

--- steps/config.py
+++ steps/config.py
@@ -1,1 +1,8 @@
-
+from pydantic import BaseModel
+class ModelNameConfig(BaseModel):
+    """
+    Model configs
+    """
+    model_name: str = "LinearRegression"
+    class Config:
+        protected_namespaces = ()
